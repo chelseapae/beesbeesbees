4f1e9193ec039d17da4681fcb9b07a001952477e
Chelsea Pae <chelseapae@gmail.com>
2022-06-21 18:54:38 -0700

    Complete ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,10 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this job = 'find pollen';
+  this.color = 'yellow';
+  this.food;
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = HoneyMakerBee;
