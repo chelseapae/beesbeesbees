3932aece2b95f630fd6852ea70ef16606efa1cba
Chelsea Pae <chelseapae@gmail.com>
2022-06-21 18:55:48 -0700

    Complete HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this job = 'make honey';
+  this.color = 'yellow';
+  this.food;
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.eat;
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot++;
+}
+HoneyMakerBee.prototype.giveHoney = function () {
+  this.honeyPot--;
+}
